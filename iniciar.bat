fe5165cfcbf7408a5dc94c349d7de18fb78b057c
wadsongs70-blip <wadsongs70@gmail.com>
2026-09-08 13:43:59 -0300

    Update iniciar.bat

--- iniciar.bat
+++ iniciar.bat
@@ -2,13 +2,14 @@
 title CL Parts Manager
 cd /d "%~dp0"
 echo ===================================================
-echo             CL PARTS MANAGER - CATALAGO TECNICO
+echo             CL PARTS MANAGER - CATALOGO TECNICO
 echo ===================================================
 echo.
-echo Iniciando servidor e abrindo o navegador...
-echo.
 set PATH=C:\Program Files\nodejs;%PATH%
-
-start http://localhost:5173
-npm run dev
+if not exist "node_modules\" (
+    echo [1/2] Instalando pacotes e dependencias...
+    call npm install
+)
+echo [2/2] Iniciando servidor e abrindo o navegador...
+call npm run dev -- --open
 pause
